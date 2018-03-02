cbfd6cd2b0723616dd4b2b5e7e8f2649a79c94b6
koolii <kuriyama@createentrance.com>
2018-03-02 13:38:21 +0100

    :+1: add PropTypes implement using Profile component

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+// もちろんこれでも動作はするんだが、Reactの機能を使うのなら下の方が良い
+// HelloFunctionalComponentのdefaultPropsで実現出来る
+const HelloManual = (props) => {
+  return <div>こんにちは、{props.name || '坂本龍馬'}さん</div>
+}
 
 const Hello = (props) => {
-  return <div>こんにちは、{props.name || '坂本龍馬'}さん</div>
+  return <div>こんにちは、{props.name}さん</div>
+}
+Hello.propTypes = {
+  name: PropTypes.string.isRequired,
+}
+Hello.defaultProps = {
+  name: '坂本龍馬',
 }
 
 export default Hello

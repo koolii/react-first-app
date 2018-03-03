d3fd7de7f5781fd12251dbe36a55260738a6744c
koolii <koolii0909@gmail.com>
2018-03-03 23:56:11 +0100

    :+1: add TodoInput component

---
+++ src/components/TodoInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class TodoInput extends Component {
+  render() {
+    return (
+      <div>
+        <input placeholder="input new todo" />
+        <button>Registeration</button>
+      </div>
+    )
+  }
+}
+
+export default TodoInput

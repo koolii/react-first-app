f53e3554dfe5f45b5a698c993be050d4b022e871
koolii <koolii0909@gmail.com>
2018-03-05 20:00:58 +0100

    :+1: add replaceReducer describe and sample

--- src/redux-flow.js
+++ src/redux-flow.js
@@ -15,12 +15,23 @@ const initialState = {
 //     task: 'Learning Reducer',
 //   }
 // }
-function tasksReducer(state = initialState, action) {
+function addReducer(state = initialState, action) {
   switch (action.type) {
     case 'ADD_TASK':
       return {
         ...state,
         tasks: state.tasks.concat([action.payload.task])
+      }
+    default:
+      return state
+  }
+}
+function resetReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'RESET_TASK':
+      return {
+        ...state,
+        tasks: [],
       }
     default:
       return state
@@ -35,28 +46,40 @@ const addTask = (task) => ({
     task,
   }
 })
+const resetTask = () => ({
+  type: 'RESET_TASK',
+})
+
 
 const reduxFlow = () => {
   // Reducerにつき、１つのStoreを管理する
   // store内にはdispatch/getState/subscribe/replaceReducerの4つがある
-  const store = createStore(tasksReducer)
+  const store = createStore(addReducer)
 
   // subscribe: Storeの状態が更新された時に、動作するコールバック関数を定義することが出来る
   // ↓のunsubscribe()を実行することでsubscribeの内容が解除される
-  const unsubscribe = store.subscribe(handleChange)
+  const addUnSubscribe = store.subscribe(handleChange)
   function handleChange() {
     // getState: Storeの状態を参照する
     const current = store.getState()
-    console.log(JSON.stringify(current))
-
-    unsubscribe()
-    console.log('unsubscribed.')
+    console.log('current store: ' + JSON.stringify(current))
   }
 
   // dispatch: Actionを発行し、それをReducerに適用、Storeを更新する
   store.dispatch(addTask(`this is an action for calling store's dispatch method`))
 
-  console.log('dispatch again (never calls dispatch method).')
+  // 動的にReducerを入れ替える(addReducer => resetReducer)
+  // replaceReducer()を実行した時に、subscribe()しているコールバックが呼び出される
+  // subscribe()してるコールバックを呼ばないようにするためにunsubscribe()を実行して再度subscribe()してあげると良い感じ
+  addUnSubscribe()
+
+  store.replaceReducer(resetReducer)
+  const resetUnSubscribe = store.subscribe(handleChange)
+
+  // 切り替えた先のActionを発行する
+  store.dispatch(resetTask())
+
+  resetUnSubscribe()
 }
 
 export default reduxFlow

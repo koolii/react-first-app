6f947c2393553daf3f5568b9ea5b05816bfa7bbd
koolii <kuriyama@createentrance.com>
2018-03-05 08:23:37 +0100

    :+1: complete TodoList which uses only React without question which is that input field can't get empty after it adds todo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import CComponents from './components/children'
 // なぜかこれが通らない
 // import { Children, Greeting } from './components/children'
 import Profile from './components/profile'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 
 const { Children, Greeting } = CComponents
 const profileKoolii = {
@@ -15,6 +17,41 @@ const profileKoolii = {
 }
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tasks: [
+        { title: 'first todo', id: 0 },
+        { title: 'second todo', id: 1 },
+      ],
+      uniqueId: 1,
+    }
+    // this(=App)のcontextを別の場所(TodoInputコンポーネント)で使用したいためbind()で束縛させる
+    // property initializer syntaxというのを使うと、関数を定義し、且つbind()された状態にすることが出来るらしい(要babel)
+    this.addTodo = this.addTodo.bind(this)
+    this.resetTodo = this.resetTodo.bind(this)
+  }
+
+  resetTodo() {
+    this.setState({
+      tasks: [],
+    })
+  }
+
+  addTodo(title) {
+    // Appのステートを管理するためのイベントだからTodoInputコンポーネントではなく、
+    // App.jsで記述し、イベント自体はTodoInputコンポーネントに渡す
+    const { tasks, uniqueId } = this.state;
+    tasks.push({
+      title,
+      id: uniqueId,
+    })
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1,
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,6 +78,13 @@ class App extends Component {
         </Greeting>
 
         <Profile {...profileKoolii} />
+
+        <div>
+          <h1>Todo App</h1>
+          <button onClick={this.resetTodo}>Reset</button>
+          <TodoInput addTodo={this.addTodo} />
+          <TodoList tasks={this.state.tasks} />
+        </div>
       </div>
     );
   }
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
 
 class TodoInput extends Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      inputValue: '',
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+    const input = this.state.inputValue
+    this.props.addTodo(input)
+
+    // [question]ここでinputタグの中身を空にしたいんだけど思ったようにならない
+    // 単純に動いていないっぽい
+    this.setState({
+      inputVaue: '',
+    })
+  }
+
+  handleChange(e) {
+    // ただ単純にinputタグのvalueにstateを当てはめて、イベントをバインドさせても
+    // ここの関数でstateのinputValueを更新させないといつまでもinputValueに値が入らないので注意
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
-        <input placeholder="input new todo" />
-        <button>Registeration</button>
+        <input placeholder="input new todo" value={this.state.inputValue} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Registeration</button>
       </div>
     )
   }

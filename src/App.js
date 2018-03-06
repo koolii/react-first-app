ff2fec518d42aa8b13bb16440253e4423078c0a5
koolii <kuriyama@createentrance.com>
2018-03-06 08:29:42 +0100

    :+1: move src/components to src/react-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactApp from './components/reactApp'
+import ReactApp from './react-components/reactApp'
 import reduxFlow from './redux-flow'
 
 

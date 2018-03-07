57129c7c55ead23d2bce2e899bacbfc80020a40c
koolii <kuriyama@createentrance.com>
2018-03-07 08:36:51 +0100

    :+1: remove unused variable in ReactComponent 'ReactReduxTodoApp'

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 // containersでStoreから必要な項目を作成し、このコンポーネントに渡されてくる
 // そのためコンポーネントでは、ただ、渡されたPropsを利用するだけで良い
-export default function ReactReduxTodoApp({ task, tasks, inputTask, addTask, name = 'react-redux' }) {
+export default function ReactReduxTodoApp({ task, tasks, inputTask, addTask }) {
   return (
     <div>
-      <h2>ReactReduxTodoApp(uses `{name}` module)</h2>
+      <h2>ReactReduxTodoApp(uses `react-redux` module)</h2>
       <input type="text" onChange={(e) => inputTask(e.target.value)} />
       <input type="button" value="add" onClick={() => { addTask(task) }} />
       <ul>

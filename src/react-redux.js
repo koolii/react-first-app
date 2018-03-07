aec2df577255967c6f8e760b6ce40cde76c57ef1
koolii <kuriyama@createentrance.com>
2018-03-07 13:12:04 +0100

    :+1: add react-router-dom and BrowserRouter component

--- src/react-redux.js
+++ src/react-redux.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import taskReducer from './reducers/tasks'
 import TodoAppContainer from './containers/TodoApp'
 
@@ -9,9 +10,12 @@ import TodoAppContainer from './containers/TodoApp'
 
 const store = createStore(taskReducer)
 
+// Routerを挟むことで下位の階層でRouteを使ったルーティングが可能になる
 ReactDOM.render(
   <Provider store={store}>
-    <TodoAppContainer />
+    <Router>
+      <TodoAppContainer />
+    </Router>
   </Provider>,
   document.getElementById('react_redux_m_root')
 )

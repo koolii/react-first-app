5fbf3b90f29f0ad1f95473ad4c39644cb659c3ae
koolii <koolii0909@gmail.com>
2018-03-08 21:54:00 +0100

    :+1: add original minimum localStorage middleware which manages all react state information

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,7 @@ import { routerReducer, routerMiddleware } from 'react-router-redux'
 import { createLogger } from 'redux-logger'
 import tasksReducer from '../reducers/tasks'
 import logger from '../middleware/logger'
+import localStorage from '../middleware/local-storage'
 
 // redux-logger
 // const logger = createLogger({
@@ -16,18 +17,26 @@ import logger from '../middleware/logger'
 //   predicate: (getState, action) => !['HIGH_FREQUENCY_ACTION', 'INPUT_TASK'].includes(action.type)
 // })
 
+// localStorage
+const savedState = JSON.parse(window.localStorage.getItem('app-state'))
+const reducers = combineReducers({
+  // tasksReducerをtasksと言うkeyに割り当てる
+  tasks: tasksReducer,
+  // ルータ用のReducer(react-router-redux)
+  router: routerReducer,
+})
+
 export default function createStore(history) {
   // reduxモジュールのcreateStore()は第二引数に初期stateの状態を渡すことが出来る（この時の第三引数がapplyMiddleware()）
   // createStore()が内部で引数の数で処理を区別してくれてる
   // applyMiddleware()はenhancerと呼ばれてサードパーティでも色々と拡張機能がある
   // それらをcombineReducers()みたいにreduxモジュールのcompose関数を使って１つにまとめる
   return reduxCreateStore(
-    combineReducers({
-      // tasksReducerをtasksと言うkeyに割り当てる
-      tasks: tasksReducer,
-      // ルータ用のReducer(react-router-redux)
-      router: routerReducer,
-    }),
+    reducers,
+    // 第二引数に最初のStateを設定することが出来る
+    // reducerの第一引数にundefinedを渡すと各reducerで定義されたinitialStateを返す
+    // 第二引数にはactionを設定するが、特に意味はなし、エラーが起こらないためだけに設定
+    savedState ? savedState : reducers(undefined, { type: 'INIT' }),
     // applyMiddleware()関数は可変長引数を受け付けて、ReduxMiddlewareを読み込ませることが出来る
     applyMiddleware(
       // ルータ用のMiddleware(react-router-redux)
@@ -36,6 +45,7 @@ export default function createStore(history) {
       // ↑のリンクで結構詳細な設定をすることができてカスタマイズできる
       routerMiddleware(history),
       logger,
+      localStorage,
     )
   )
 }
